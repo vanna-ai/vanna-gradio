ec45ae9eb9cdc58131f1ed92e34dc83d26a8e228
André Pedersen <andrped94@gmail.com>
2024-03-12 19:50:06 +0000

    Added buttons to clear chat and code editor

--- demo/ui.py
+++ demo/ui.py
@@ -1,6 +1,7 @@
 import os
 
 import gradio as gr
+import vanna as vn
 
 from .utils import add_prompt_to_history, event_handler
 
@@ -8,6 +9,9 @@ from .utils import add_prompt_to_history, event_handler
 class WebUI:
     def __init__(self):
         self.history = []
+        self.current_question = None
+        self.current_code = ""
+        self.indices = []
         self.create_components()
 
     def create_components(self):
@@ -17,7 +21,7 @@ class WebUI:
             bubble_full_width=False,
             avatar_images=(None, (os.path.join(os.path.abspath(""), "avatar.png"))),
             rtl=False,
-            label=True,
+            label="Chatbot",
             height=800,
         )
 
@@ -26,23 +30,83 @@ class WebUI:
             show_label=False,
             placeholder="What are the top 10 albums by sales?",
             container=False,
+            elem_id="textbox",
         )
 
         self.code_editor = gr.Code(
             interactive=True,
             lines=51,
+            # every=1,
+            value=self.update_code,
+            language="sql",
+            elem_id="code_editor",
         )
 
-        self.plot = gr.Plot()
+        self.clear_chat_button = gr.Button(
+            value="Clear chat",
+            variant="secondary",
+            elem_id="clear_chat",
+        )
 
-    def like_dislike_event(self):
-        pass
+        self.clear_code_button = gr.Button(
+            value="Clear code",
+            variant="secondary",  # "stop"
+            elem_id="clear_code",
+        )
+
+        self.save_button = gr.Button(
+            value="Save question-sql pair",
+            variant="primary",
+            elem_id="save_pair",
+        )
+
+    def update_code(self):
+        return self.current_code
+
+    def get_vote(self, data: gr.LikeData, history):
+        if data is None:
+            return
+
+        # ignore thumbs up events
+        if data.liked:
+            print("Thumbs up event happened (but did nothing!)")
+            return history
+
+        print(data.value)
+
+        # handle if SQL string
+        if isinstance(data.value, str) and ("SELECT" in data.value):
+            self.current_code = data.value
+            return history
+
+        current = data.value["file"]["mime_type"]
+
+        if current == "image/jpeg":
+            pass
+
+    def train_on_prompt_sql(self):
+        print("training vanna...")
+        vn.train(question=self.current_question, sql=self.current_code)
+        print("Done!")
+
+    def clear_chat(self, history):
+        self.current_question = None
+        self.clear_code()  # also clears code
+        return []
+
+    def clear_code(self):
+        self.current_code = ""
 
     def run(self):
         with gr.Blocks() as demo:
             with gr.Row():
                 # chatbot column
                 with gr.Column():
+                    self.clear_chat_button.render()
+                    self.clear_chat_button.click(
+                        self.clear_chat, self.chatbot, self.chatbot
+                    )
+
                     self.chatbot.render()
                     self.question.render()
 
@@ -64,11 +128,25 @@ class WebUI:
                         queue=False,
                     )
 
-                    self.chatbot.like(self.like_dislike_event, None, None)
+                    self.chatbot.like(
+                        self.get_vote, [self.chatbot], [self.chatbot]
+                    )  # .then(self.get_vote, None, self.code_editor)
 
                 # code correction column
                 with gr.Column():
+                    with gr.Row():
+                        self.clear_code_button.render()
+                        self.clear_code_button.click(self.clear_code)
+
+                        self.save_button.render()
+                        self.save_button.click(self.train_on_prompt_sql)
+
                     self.code_editor.render()
+                    self.code_editor.focus(self.update_code, None, self.code_editor)
+
+                    # self.code_editor.input(self.update_code, None, None)
+
+                    # interface = gr.Interface(fn=self.get_vote, inputs=self.chatbot, outputs=self.code_editor)
 
         demo.queue().launch(
             server_name="0.0.0.0",
