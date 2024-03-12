5b0f13339c619d2a4a24b81b57f8f2eedfc521a0
André Pedersen <andrped94@gmail.com>
2024-03-12 19:49:28 +0000

    Add warning if key not found

--- demo/setup.py
+++ demo/setup.py
@@ -9,11 +9,18 @@ def setup_vanna(vn):
 
     if "EMAIL" in os.environ:
         vn = VannaDefault(model="chinook", api_key=vn.get_api_key(os.getenv("EMAIL")))
+        vn.connect_to_sqlite("https://vanna.ai/Chinook.sqlite")
     elif "VANNA_API_KEY" in os.environ:
         vn = VannaDefault(
             model="chinook", api_key=vn.get_api_key(os.getenv("VANNA_API_KEY"))
         )
+        vn.connect_to_sqlite("https://vanna.ai/Chinook.sqlite")
+    else:
+        vn = VannaDefault(model="chinook", api_key=None)
+        vn.connect_to_sqlite("Chinook.sqlite")
 
-    vn.connect_to_sqlite("https://vanna.ai/Chinook.sqlite")
+        print(
+            "WARNING: Neither EMAIL nor VANNA_API_KEY were found in the '.env' file. Setting api_key=None."
+        )
 
     return vn
